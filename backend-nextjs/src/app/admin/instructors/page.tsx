a18e2b3b9b0353d18ceea635d319be68b0afce73
Utkarsh <a.swaranakar@gmail.com>
2026-04-01 09:19:28 -0700

    Add backend admin instructors route

---
+++ backend-nextjs/src/app/admin/instructors/page.tsx
@@ -0,0 +1,7 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+export default function InstructorsAdminPage() {
+  const html = readFileSync(join(process.cwd(), 'public/admin/instructors.html'), 'utf-8');
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+}
